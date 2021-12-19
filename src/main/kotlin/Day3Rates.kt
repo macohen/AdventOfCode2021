ae58548689bd9d8cb34cace166df04e66bf318b6
Mark Cohen <markcoh@amazon.com>
2021-12-19 11:59:31 -0500

    refactoring for readability, introduced paramter functions

--- src/main/kotlin/Day3Rates.kt
+++ src/main/kotlin/Day3Rates.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.reflect.KFunction2
 
 fun readRatesTestInput(fileName: String): List<String> {
     val moveList = mutableListOf<String>()
@@ -24,99 +25,83 @@ fun calculatePowerConsumption(binaryNums: List<String>): Int {
 }
 
 fun calculateOxygenGeneratorRating(binaries: List<String>): Int {
-    var reducedBinaryList: MutableList<String> = binaries as MutableList<String>
-    val binaryLength = binaries[0].length
-    for (digitPosition in 0 until binaryLength) {
-        if (reducedBinaryList.size == 1) {
-            break
-        }
-        val binariesWithOneAtPosition: MutableList<String> = mutableListOf()
-        val binariesWithZeroAtPosition: MutableList<String> = mutableListOf()
-        for (binary in reducedBinaryList) {
-            if (binary[digitPosition] == '0') {
-                binariesWithZeroAtPosition.add(binary)
-            } else {
-                binariesWithOneAtPosition.add(binary)
-            }
-        }
-        reducedBinaryList = if (binariesWithOneAtPosition.size >= binariesWithZeroAtPosition.size) {
-            binariesWithOneAtPosition
-        } else {
-            binariesWithZeroAtPosition
-        }
-    }
+    val reducedBinaryList: MutableList<String> = reduceBinaries(binaries, ::selectO2GeneratorRating)
     return calculateDecimal(reducedBinaryList.get(0))
 }
 
-fun calculateCO2ScrubberRating(binaries: List<String>): Int {
-    var reducedBinaryList: MutableList<String> = binaries as MutableList<String>
-    val binaryLength = binaries[0].length
-    for (digitPosition in 0 until binaryLength) {
-        if (reducedBinaryList.size == 1) {
-            break
+fun selectO2GeneratorRating(l1: MutableList<String>, l2: MutableList<String>): MutableList<String> = if (l1.size >= l2.size) l1 else l2
+fun selectCO2Rating(l1: MutableList<String>, l2: MutableList<String>): MutableList<String> = if (l2.size <= l1.size) l2 else l1
+
+    private fun reduceBinaries(binaries: List<String>, comparator: KFunction2<MutableList<String>, MutableList<String>, MutableList<String>>): MutableList<String> {
+        var reducedBinaryList: MutableList<String> = binaries as MutableList<String>
+        val binaryLength = binaries[0].length
+        for (digitPosition in 0 until binaryLength) {
+            if (reducedBinaryList.size == 1) {
+                break
+            }
+            val binariesWithOneAtPosition: MutableList<String> = mutableListOf()
+            val binariesWithZeroAtPosition: MutableList<String> = mutableListOf()
+            for (binary in reducedBinaryList) {
+                if (binary[digitPosition] == '0') {
+                    binariesWithZeroAtPosition.add(binary)
+                } else {
+                    binariesWithOneAtPosition.add(binary)
+                }
+            }
+            reducedBinaryList = comparator(binariesWithOneAtPosition, binariesWithZeroAtPosition)
         }
-        val binariesWithOneAtPosition: MutableList<String> = mutableListOf()
-        val binariesWithZeroAtPosition: MutableList<String> = mutableListOf()
-        for (binary in reducedBinaryList) {
-            if (binary[digitPosition] == '0') {
-                binariesWithZeroAtPosition.add(binary)
-            } else {
-                binariesWithOneAtPosition.add(binary)
+        return reducedBinaryList
+    }
+    fun calculateCO2ScrubberRating(binaries: List<String>): Int {
+        val reducedBinaryList: MutableList<String> = reduceBinaries(binaries, ::selectCO2Rating)
+        return calculateDecimal(reducedBinaryList.get(0))
+    }
+
+    fun selectMostCommon(onesCount: Int, zeroesCount: Int) :Char = if (onesCount > zeroesCount) '0' else '1'
+    fun selectLeastCommon(onesCount: Int, zeroesCount: Int) :Char = if (onesCount > zeroesCount) '1' else '0'
+
+
+    fun calculateRate(binaryNums: List<String>, appendToFinalBinary: (Int, Int) -> Char): String {
+        var onesCount = 0
+        var zerosCount = 0
+        val binaryLength = binaryNums[0].length - 1
+        var finalBinary = ""
+        for (digitPosition in 0..binaryLength) {
+            for (binary in binaryNums) {
+                val digit = binary[digitPosition]
+                if (digit == '0') zerosCount++ else onesCount++
+            }
+            finalBinary += appendToFinalBinary(onesCount, zerosCount)
+            onesCount = 0
+            zerosCount = 0
+        }
+        return finalBinary
+    }
+
+    fun calculateEpsilonRate(binaryNums: List<String>): Int {
+        val binaryRate = calculateRate(binaryNums, ::selectMostCommon)
+        return calculateDecimal(binaryRate)
+    }
+
+    fun calculateGammaRate(binaryNums: List<String>): Int {
+        val binaryRate = calculateRate(binaryNums, ::selectLeastCommon)
+        return calculateDecimal(binaryRate)
+    }
+
+    private fun calculateDecimal(binary: String): Int {
+        var decimalNumber = 0
+        var num: Long? = binary.toLongOrNull()
+        if (num != null) {
+
+            var i = 0
+            var remainder: Long
+            while (num.toInt() != 0) {
+                remainder = num % 10
+                num /= 10
+                decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
+                ++i
             }
         }
-        reducedBinaryList = if (binariesWithZeroAtPosition.size <= binariesWithOneAtPosition.size) {
-            binariesWithZeroAtPosition
-        } else {
-            binariesWithOneAtPosition
-        }
+        return decimalNumber
     }
-    return calculateDecimal(reducedBinaryList.get(0))
-}
 
-fun calculateRate(binaryNums: List<String>, type: String): String {
-    var onesCount = 0
-    var zerosCount = 0
-    val binaryLength = binaryNums[0].length - 1
-    var finalBinary = ""
-    for (digitPosition in 0..binaryLength) {
-        for (binary in binaryNums) {
-            val digit = binary[digitPosition]
-            if (digit == '0') zerosCount++ else onesCount++
-        }
-        if (type == "mostCommon") {
-                finalBinary += (if (onesCount > zerosCount) '0' else '1')
-        } else if(type == "leastCommon") {
-                finalBinary += (if (onesCount > zerosCount) '1' else '0')
-        }
-        onesCount = 0
-        zerosCount = 0
-    }
-    return finalBinary
-}
-
-fun calculateEpsilonRate(binaryNums: List<String>): Int {
-    val binaryRate = calculateRate(binaryNums, type="mostCommon")
-    return calculateDecimal(binaryRate)
-}
-
-fun calculateGammaRate(binaryNums: List<String>): Int {
-    val binaryRate = calculateRate(binaryNums, type="leastCommon")
-    return calculateDecimal(binaryRate)
-}
-
-private fun calculateDecimal(binary: String): Int {
-    var decimalNumber = 0
-    var num: Long? = binary.toLongOrNull()
-    if (num != null) {
-
-        var i = 0
-        var remainder: Long
-        while (num.toInt() != 0) {
-            remainder = num % 10
-            num /= 10
-            decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
-            ++i
-        }
-    }
-    return decimalNumber
-}
